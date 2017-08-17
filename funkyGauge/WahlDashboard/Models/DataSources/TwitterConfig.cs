1b247b28516c1a148eea5c949cef40a1aec4e461
wwirsam <wirsam@intuisoft.de>
2017-08-17 17:47:44 +0200

    ignore TwitterConfig user secret settings

--- funkyGauge/WahlDashboard/Models/DataSources/TwitterConfig.cs
+++ funkyGauge/WahlDashboard/Models/DataSources/TwitterConfig.cs
@@ -6,7 +6,7 @@ namespace WahlDashboard.Models.DataSources
     {
         public static void InitApp()
         {
-            Auth.SetUserCredentials("YourConsumerKey", "YourConsumerSecret", "yourUserAccessToken", "yourUserAccessSecret");
+            Auth.SetUserCredentials("","","","");
         }
     }
 }
